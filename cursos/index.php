c2693c7161c8672f17ad4fd9a85e9a0fd35e1c4f
jorge <jorgedipra@gmail.com>
2013-04-27 06:54:07 -0500

    aumento de velocidad 20%
    aun esta lento , falta aterrizar cosas

--- cursos/index.php
+++ cursos/index.php
@@ -14,11 +14,52 @@
 <meta property="og:site_name" content=" " />
 <title>CursoVirtual</title>
 <link rel="shortcut icon" href="favicon.ico" />
+<link   href="css/StyleBarra.css" rel="stylesheet" /> 
+<link href='http://fonts.googleapis.com/css?family=Shojumaru' rel='stylesheet'>
+<script src="script/jquery.min.js" class="cssdeck"></script>
+<script src="script/basicoescritorio.js"></script>	
+<div id="taskbar" >
+     <div class="copyright ss-icon">
+     	<span id="tituloOs">OS </span>
+     	<span id="titulo"> Escritorio de Aprendizaje</span>
+	   <section id="tituloHer">
+	   <a class="Escritorio name" href="#newbar" onclick="javascript:fun_iFrameWrite('iFrame_SQL','img/null.gif')">
+	   <img src="img/Desktop.png">
+	   </a>
+	   <a class="Informacion name" href="#newbar" onclick="javascript:fun_iFrameWrite('iFrame_SQL','/url')">
+	   	<img src="img/Informacion.png">
+	   </a>
+	   <a class="Herramientas name" href="#newbar" onclick="javascript:fun_iFrameWrite('iFrame_SQL','/url')">
+	   	<img src="img/Herramienta.png">
+	   </a>
+       </section>
+		</div>
+		<div id="clock"></div>
+		<div id="logout"><a href="#">Home</a></div>
+		<div id="preview"><a href="javascript:history.back(1)">Atras</a></div>
+</div>
+<link   href="css/StyleEscritorio.css" rel="stylesheet" /> 
+<script src="script/escritorio.js "></script>
 </head>
-<frameset rows="1,*" frameborder="no" border="0" framespacing="0">
-  <frame src="" name="top" scrolling="No" noresize="noresize" id="top" title="top" />
-  <frame src="home.php" name="contenido" id="contenido" title="home" />
-</frameset>
-<noframes><body>
-</body></noframes>
+<body onload="javascript:fun_iFrameWrite('iFrame_SQL','')"> 
+<iframe id="iFrame_SQL" name="iFrame_SQL"></iframe>	
+	<div id="main">
+
+<a class="name"href="#newbar" onclick="javascript:fun_iFrameWrite('iFrame_SQL','cursos/basico/index.php')">
+		<div id="carpeta1" class="icon">
+			Basico
+		</div>
+</a>	
+<a class="name"href="#newbar" onclick="javascript:fun_iFrameWrite('iFrame_SQL','cursos/Medio/index.php')">	
+		<div id="carpeta2" class="icon">
+			Medio
+		</div>
+</a>		
+<a class="name"href="#newbar" onclick="javascript:fun_iFrameWrite('iFrame_SQL','cursos/Avanzado/index.php')">		
+		<div id="carpeta3" class="icon">
+			Avanzado
+		</div>
+</a>	
+	</div>	
+</body>
 </html>
